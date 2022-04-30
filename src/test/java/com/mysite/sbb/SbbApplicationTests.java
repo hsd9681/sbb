713deadee5bd270472c1ae2aa3f793b82045db03
hsd9681 <hsd9681@naver.com>
2022-04-30 16:50:03 +0900

    리포지터리, 데이터 조회하기, findBySubjectAndContent

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -16,6 +16,12 @@ class SbbApplicationTests {
 	@Autowired
 	private QuestionRepository questionRepository;
 
+	@Test
+	void testFindBySubjectAndContent() {
+		Question q = questionRepository.findBySubjectAndContent("sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
+		assertEquals(q.getId(), 1);
+	}
+
 
 	@Test
 	void testFindBySubject() {
